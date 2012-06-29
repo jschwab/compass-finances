7d131d66a7472bfe3e383d17948f7222f17bad83
Josiah Schwab <jwschwab@berkeley.edu>
2012-06-29 11:48:56 -0700

    Define data models for gifts

--- gifts/models.py
+++ gifts/models.py
@@ -1,10 +1,64 @@
 from django.db import models
 
+RELATIONSHIP_CHOICES = (
+    ('FRIEND', 'Friend'),
+    ('FAMLIY', 'Family'),
+    ('ALUMNUS', 'Alumnus/Alumna'),
+    ('STUDENT', 'Student'),
+)
+
+
 class Contact(models.Model):
-    pass
+
+    # essential info
+    name = models.CharField(max_length = 255)
+
+    # contact info
+    street = models.CharField(max_length = 255, blank = True)
+    city = models.CharField(max_length = 127, blank = True)
+    state = models.CharField(max_length = 127, blank = True)
+    postcode = models.CharField(max_length = 63, blank = True)
+    country = models.CharField(max_length = 63, blank = True)
+    email = models.EmailField(max_length = 254, blank = True)
+
+    # interesting info
+    relationship = models.CharField(max_length = 15, blank = True,
+                                    choices = RELATIONSHIP_CHOICES)
+    point_person = models.CharField(max_length = 127, blank = True)
+    comment = models.CharField(max_length = 2047, blank = True)
+
+    def __unicode__(self):
+        return self.name
 
 class Donation(models.Model):
-    pass
+
+    # essential info
+    contact = models.ForeignKey('Contact')
+    date = models.DateField()
+    amount = models.DecimalField(max_digits = 12, decimal_places = 2)
+
+    # matching information
+    matching_funds = models.BooleanField(default = False)
+    matching_source = models.CharField(max_length = 255, blank = True)
+
+    # donor-provided info
+    publish = models.BooleanField(default = False)    
+    comments = models.CharField(max_length = 2047, blank = True)
+    referrer = models.CharField(max_length = 255, blank = True)
+
+    # administrative info
+    confirmation_number = models.IntegerField(blank=True,null=True)
+    fund_number = models.CharField(max_length = 31, null = True, blank = True)
+
+    def __unicode__(self):
+        return "{} - {}".format(self.name, self.date)
 
 class Ask(models.Model):
-    pass
+
+    contact = models.ForeignKey('Contact')
+    date = models.DateField()
+    campaign = models.CharField(max_length = 255, blank = True)
+    notes = models.CharField(max_length = 2047, blank = True)
+    
+    def __unicode__(self):
+        return self.campaign
