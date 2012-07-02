9cd3096db973fab54921b80524d80188e181dbd4
Josiah Schwab <jwschwab@berkeley.edu>
2012-07-02 12:30:25 -0700

    turn of admindocs and redirect homepage

--- finances/urls.py
+++ finances/urls.py
@@ -7,11 +7,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'finances.views.home', name='home'),
+    url(r'^$', 'finances.views.home', name='home'),
     url(r'^gifts/', include('gifts.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls), name = 'admin'),
